e95ea38676a10b70d59120e112f00749b43c7bed
Al-Amin Islam <jimalamin7@gmail.com>
2026-05-06 21:51:54 +0600

    fix issue

--- server/server.js
+++ server/server.js
@@ -44,13 +44,14 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://imagify-ai-generate-3223.netlify.app",
+      process.env.FRONTEND_URL,
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 app.use(express.json());
 app.use(morgan("dev"));
